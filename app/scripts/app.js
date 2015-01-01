206bcbd097be498d3f5cdaecd9f5ea10931b7870
Jimmi Dyson <jimmidyson@gmail.com>
2015-01-01 20:00:14 +0000

    Working perspective selection

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,14 +8,15 @@
     console.log('Polymer is ready to rock!');
 
     template.perspectives = [
-      {label: "Demo", href: '/demo/1234', page: '/pages/demo-page.html', icon: 'accessibility', path: '/demo/:pathArg1'},
-      {label: "Fabric8", href: '/', page: '/pages/home-page.html', path: '/', image: 'https://issues.jboss.org/secure/attachment/12384407/fabric8_500x400.png'}
+      {label: "Fabric8", href: '/', page: '/pages/home-page.html', path: '/', image: 'https://issues.jboss.org/secure/attachment/12384407/fabric8_500x400.png'},
+      {label: "Demo", href: '/demo/1234', page: '/pages/demo-page.html', icon: 'accessibility', path: '/demo/:pathArg1'}
     ];
 
     template.appRouterStateChange = function(event) {
-      for (var i = 0; i < template.perspectives.length; i++) {
-        if (template.perspectives[i].href === event.detail.path) {
-          template.currentPerspective = template.perspectives[i];
+      var tmpPerspectives = _.sortBy(template.perspectives, function(p) {return -(p.href.length);});
+      for (var i = 0; i < tmpPerspectives.length; i++) {
+        if (event.detail.path.slice(0, tmpPerspectives[i].href.length) === tmpPerspectives[i].href) {
+          template.currentPerspective = tmpPerspectives[i];
           return;
         }
       }
