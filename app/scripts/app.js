cf875e2890f73422b9a7cf862de8513d7578de10
Jimmi Dyson <jimmidyson@gmail.com>
2015-01-02 15:40:54 +0000

    Working perspective switcher & better layout

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,25 +3,37 @@
 
   var template = document.querySelector('#t');
 
-  document.addEventListener('polymer-ready', function() {
-    // Perform some behaviour
-    console.log('Polymer is ready to rock!');
+  template.addEventListener('template-bound', function(e) {
+    template.perspectives = window.Perspectives;
 
-    template.perspectives = [
-      {label: "Fabric8", href: '/', page: '/pages/home-page.html', path: '/', image: 'https://issues.jboss.org/secure/attachment/12384407/fabric8_500x400.png'},
-      {label: "Demo", href: '/demo/1234', page: '/pages/demo-page.html', icon: 'accessibility', path: '/demo/:pathArg1'}
-    ];
+    template.branding = window.Branding;
 
-    template.appRouterStateChange = function(event) {
-      var tmpPerspectives = _.sortBy(template.perspectives, function(p) {return -(p.href.length);});
-      for (var i = 0; i < tmpPerspectives.length; i++) {
-        if (event.detail.path.slice(0, tmpPerspectives[i].href.length) === tmpPerspectives[i].href) {
-          template.currentPerspective = tmpPerspectives[i];
-          return;
-        }
+    template.router = document.querySelector('#perspectiveRouter');
+
+    var tmpPerspectives = _.sortBy(template.perspectives, function(p) {return -(p.href.length);});
+    for (var i = 0; i < tmpPerspectives.length; i++) {
+      if (location.pathname.slice(0, tmpPerspectives[i].href.length) === tmpPerspectives[i].href) {
+        template.currentPerspective = tmpPerspectives[i];
+        break;
       }
     }
+
+    template.selectedPerspectiveIndex = _.indexOf(_.pluck(template.perspectives, 'href'), template.currentPerspective.href);
   });
+
+  template.onPerspectiveSelect = function(event, detail) {
+    if (detail.isSelected) {
+      this.$ && this.$.drawerPanel.togglePanel();
+      template.currentPerspective = detail.item.templateInstance.model.perspective;
+      if (location.pathname === template.currentPerspective.href) {
+        template.router.go(template.currentPerspective.href);
+      }
+    }
+  }
+
+  template.onPerspectiveTapped = function(event, detail) {
+    template.router.go(template.currentPerspective.href);
+  }
 
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
