d7827471eb044bc0a980989bbeaf78c2d0f86d12
Jimmi Dyson <jimmidyson@gmail.com>
2015-01-02 20:37:36 +0000

    Use openshift versions of express

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ var port    = process.env.OPENSHIFT_NODEJS_PORT || 9000;
 var express = require('express')
 var app = express();
 
+app.use(express.logger());
+app.use(express.compress());
+
 app.use(express.static(__dirname));
-app.use('/*', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.all('/*', function(req, res){
+  res.sendfile(__dirname + '/index.html');
 });
 
 app.listen(port, ipaddr);
